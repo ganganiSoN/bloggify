1af6093a585858f79b0779e712c5466dce18a69d
GanganiSoN <ganganiroshan97@gmail.com>
2022-01-02 13:48:54 +0530

    change the dashbboard ui

--- src/component/dashboard/Dashboard.tsx
+++ src/component/dashboard/Dashboard.tsx
@@ -6,7 +6,9 @@ class Dashboard extends React.Component {
     data = [
         {
             id: 1,
-            img: 'https://picsum.photos/200/300?random=2',
+            author: 'Stone John',
+            authorImage: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg',
+            img: 'https://picsum.photos/200/150?random=2',
             question: 'What is SEO? An explanation for beginners',
             blogText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
             date: '3 May 2019',
@@ -16,7 +18,9 @@ class Dashboard extends React.Component {
         },
         {
             id: 2,
-            img: 'https://picsum.photos/200/300?random=2',
+            author: 'Priya Ponnappa',
+            authorImage: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg',
+            img: 'https://picsum.photos/200/150?random=2',
             question: 'What is SEO? An explanation for beginners',
             blogText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
             date: '3 May 2019',
@@ -26,7 +30,9 @@ class Dashboard extends React.Component {
         },
         {
             id: 3,
-            img: 'https://picsum.photos/200/300?random=2',
+            author: 'Wong Mia',
+            authorImage: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg',
+            img: 'https://picsum.photos/200/150?random=2',
             question: 'What is SEO? An explanation for beginners',
             blogText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
             date: '3 May 2019',
@@ -36,7 +42,9 @@ class Dashboard extends React.Component {
         },
         {
             id: 4,
-            img: 'https://picsum.photos/200/300?random=2',
+            author: 'Stanbrige Peter',
+            authorImage: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg',
+            img: 'https://picsum.photos/200/150?random=2',
             question: 'What is SEO? An explanation for beginners',
             blogText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
             date: '3 May 2019',
@@ -46,7 +54,9 @@ class Dashboard extends React.Component {
         },
         {
             id: 5,
-            img: 'https://picsum.photos/200/300?random=2',
+            author: 'Lee-Walsh Natalie',
+            authorImage: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg',
+            img: 'https://picsum.photos/200/150?random=2',
             question: 'What is SEO? An explanation for beginners',
             blogText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
             date: '3 May 2019',
@@ -67,18 +77,22 @@ class Dashboard extends React.Component {
                         this.data.map((v) => 
                             <div className="container">    
                                 <div className="blog-detail">
-                                    <img src={v.img} alt="blog"></img>
+                                    <img className='blog-image' src={v.img} alt="blog"></img>
                                     <div className="blog-text-detail">
+                                        <div className='author-detail'>
+                                            <img className='avtar-image' src={v.authorImage} alt="avtar"></img>
+                                            <span className='text-overflow-ellipsis arial-font font-weight-500'> {v.author} </span>
+                                        </div>
                                         <h2><a href={`detail-blog-view/${v.id}`}>{v.question}</a></h2>
-                                        <span className="blog-detail-text"> {v.blogText} </span>
+                                        <p className="blog-detail-text arial-font"> {v.blogText} </p>
                                         <div className="blog-view-detail">
-                                            <span> {v.date} </span>
+                                            <span className="arial-font font-weight-500"> {v.date} </span>
+                                            <span className="arial-font font-weight-500">|</span>
+                                            <span className="arial-font font-weight-500"> {v.comment} comments </span>
                                             <span>|</span>
-                                            <span> {v.comment} comments </span>
+                                            <span className="arial-font font-weight-500"> {v.view} view </span>
                                             <span>|</span>
-                                            <span> {v.view} view </span>
-                                            <span>|</span>
-                                            <span> {v.category} </span>
+                                            <span className="arial-font font-weight-500"> {v.category} </span>
                                         </div>
                                     </div>
                                 </div>
--- src/component/detail-blog-view/Detail-Blog-View.tsx
+++ src/component/detail-blog-view/Detail-Blog-View.tsx
@@ -7,23 +7,37 @@ class DetailBlogView extends React.Component {
             <React.Fragment>
                 <section id="detailBlogView">
 
+                    {/* blog heading section */}
                     <section className="heading-section">
                         
                         <h1>What is SEO?</h1>
 
                         <div>
 
-                            <label>3 May 2019 </label>
+                            <label className="font-weight-500">3 May 2019 </label>
                              |
-                            <label> 9 comments </label>
+                            <label className="font-weight-500"> 9 comments </label>
                             |
-                            <label> 999 views </label>
+                            <label className="font-weight-500"> 999 views </label>
                             |
-                            <label> Category </label>
+                            <label className="font-weight-500"> Category </label>
 
                         </div>
 
-                        
+                    </section>
+
+                    {/* blog body section */}
+                    <section className="body-section">
+
+                        <img src="https://i.picsum.photos/id/1001/5616/3744.jpg?hmac=38lkvX7tHXmlNbI0HzZbtkJ6_wpWyqvkX4Ty6vYElZE" width="100%" height="300px" alt="blog-image"></img>
+
+                        <div className="body-detail">
+
+                            What is Lorem Ipsum?
+
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                        </div>
 
                     </section>
 
