bf86099cd3eebb185d6296cb82e6807afef3ca52
GanganiSoN <ganganiroshan97@gmail.com>
2022-01-23 17:16:33 +0530

    change in detail view

--- src/component/detail-blog-view/Detail-Blog-View.tsx
+++ src/component/detail-blog-view/Detail-Blog-View.tsx
@@ -23,7 +23,24 @@ class DetailBlogView extends React.Component {
 
                         </div>
 
-                        
+                    </section>
+
+                    {/* blog body section */}
+                    <section className="body-section">
+
+                        <img src="https://i.picsum.photos/id/1001/5616/3744.jpg?hmac=38lkvX7tHXmlNbI0HzZbtkJ6_wpWyqvkX4Ty6vYElZE" width="100%" height="300px" alt="blog-image"></img>
+
+                        <div className="body-detail">
+
+                            <p>
+
+                                What is Lorem Ipsum?
+
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                            </p>
+
+                        </div>
 
                     </section>
 
